db817012454314763f46533b59c38b15d0828987
Anton Kochanov <129223554+st1mka48@users.noreply.github.com>
2023-05-22 17:08:47 +0300

    5_while2.py

--- 5_while2.py
+++ 5_while2.py
@@ -12,16 +12,31 @@
 
     Пользователь: Что делаешь?
     Программа: Программирую
-    
+
 """
 
 questions_and_answers = {}
+
+the_end = ''
+while the_end != 'n':
+    your_question = input('Вопрос: ')
+    questions_and_answers[your_question] = input('Ответ: ')
+    the_end = input('Продолжить?' 'y/n ')
+
 
 def ask_user(answers_dict):
     """
     Замените pass на ваш код
     """
-    pass
-    
+    finish = ''
+    while finish != 'n':
+        try:
+            search = answers_dict[input('Ваш вопрос? ')]
+            print(search)
+            finish = input('Продолжить?' 'y/n ')
+        except KeyError:
+            print('Вопрос не найден')
+
+
 if __name__ == "__main__":
     ask_user(questions_and_answers)
